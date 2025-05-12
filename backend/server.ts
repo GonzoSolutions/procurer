7dc0d2909d31f67ffb9a72e363f3a644f4ecec7d
daniel <daniel.hermansen@proton.me>
2025-05-12 18:52:11 +0200

    added preliminary code to setup database

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,32 @@
 // backend/server.ts
 //
 
+import { Database } from "bun:sqlite";
+
+//
+//
+
+var db: Database = undefined;
+db = new Database("env/db.sqlite", { create: true, strict: true });
+
+//
+//
+
+var query: any = undefined;
+var result: any = undefined;
+
+//
+//
+
+query = await db.query("CREATE TABLE IF NOT EXISTS vendors (id INTEGER PRIMARY KEY, name TEXT NOT NULL)");
+result = await query.run();
+
+query = await db.query("CREATE TABLE IF NOT EXISTS emails (id INTEGER PRIMARY KEY, name TEXT NOT NULL)");
+result = await query.run();
+
+//
+//
+
 const server = Bun.serve(
   {
     port: 8192, 
@@ -16,8 +42,6 @@ const server = Bun.serve(
     }
   }
 );
-
-
 
 //
 //
