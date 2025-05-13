cb40c8142b592305ad370fe9350c413aeb9b4ecb
daniel <daniel.hermansen@proton.me>
2025-05-13 15:13:20 +0200

    created new dashboard using vite

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,31 +1,10 @@
-//
-// main.tsx
-//
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-//
-//
-
-import App from "./app";
-
-//
-//
-
-const element = document.createElement('div');
-element.id = 'root';
-
-//
-//
-
-document.body.append(element);
-
-//
-//
-
-const root = ReactDOM.createRoot(element);
-root.render(<App />);
-
-//
-//
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
